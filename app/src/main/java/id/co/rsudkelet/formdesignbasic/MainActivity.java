9bb81426554e59f7e1541c31303b6ce2d694c189
Agtian Muhamad Ricky Tanshidiq <agtianrickey33@gmail.com>
2022-09-02 14:56:30 +0700

    first commit

---
+++ app/src/main/java/id/co/rsudkelet/formdesignbasic/MainActivity.java
@@ -0,0 +1,36 @@
+package id.co.rsudkelet.formdesignbasic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        // return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item_done) {
+            Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
